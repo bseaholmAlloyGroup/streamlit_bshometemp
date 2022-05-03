f966e822b77226f3f202b23ad8f12ae7d791878d
bseaholmAlloyGroup <bseaholm@alloygroup.com>
2022-05-02 19:15:08 -0600

    Update bshometemp.py

--- bshometemp.py
+++ bshometemp.py
@@ -27,44 +27,40 @@ def get_data_from_table_storage_table(table_service, filter_query):
     ):
         yield record
 
-#Date picker
-start_date = st.date_input(
-     "Select start date",
-     datetime(datetime.now().year,datetime.now().month,datetime.now().day - 1))
-stop_date = st.date_input(
-     "Select stop date",
-     datetime.now())
+
+with st.expander("Home Temperature Expander"):
+    
+    #Date picker
+    start_date = st.date_input(
+        "Select start date",
+        datetime(datetime.now().year,datetime.now().month,datetime.now().day - 1))
+    stop_date = st.date_input(
+        "Select stop date",
+        datetime.now())
 
 
-#Filter and retrieve Azure Storage Table
-fq = "Timestamp gt datetime'" + start_date.strftime("%Y-%m-%d") + "T00:00:00.0000000Z'" + " and Timestamp lt datetime'" + stop_date.strftime("%Y-%m-%d") + "T00:00:00.0000000Z'"
-#fq = ''
-ts = set_table_service()
-df = get_dataframe_from_table_storage_table(table_service=ts,
-                                            filter_query=fq)
+    #Filter and retrieve Azure Storage Table
+    fq = "Timestamp gt datetime'" + start_date.strftime("%Y-%m-%d") + "T00:00:00.0000000Z'" + " and Timestamp lt datetime'" + stop_date.strftime("%Y-%m-%d") + "T00:00:00.0000000Z'"
+    #fq = ''
+    ts = set_table_service()
+    df = get_dataframe_from_table_storage_table(table_service=ts,
+                                                filter_query=fq)
 
-#Clean up Pandas dataframe for charting in Altair
-df = df.drop(labels="PartitionKey", axis=1)
-df = df.drop(labels="RowKey", axis=1)
-df = df.drop(labels="Humidity", axis=1)
-df = df.drop(labels="etag", axis=1)
-df["Timestamp"] = pd.to_datetime(df["Timestamp"])
-df["Temperature"] = pd.to_numeric(df["Temperature"])
+    #Clean up Pandas dataframe for charting in Altair
+    df = df.drop(labels="PartitionKey", axis=1)
+    df = df.drop(labels="RowKey", axis=1)
+    df = df.drop(labels="Humidity", axis=1)
+    df = df.drop(labels="etag", axis=1)
+    df["Timestamp"] = pd.to_datetime(df["Timestamp"])
+    df["Temperature"] = pd.to_numeric(df["Temperature"])
 
-#Chart in Altair and display in Streamlit
-chart = alt.Chart(df).mark_point().encode(
-    x='Timestamp:T',
-    y='Temperature:Q',
-    tooltip=[alt.Tooltip('Timestamp', timeUnit="hoursminutesseconds"), 'Temperature']
-).properties(
-    width=750,
-    height=550
-)
-
-with st.expander("See explanation"):
-     st.write("""
-         The chart above shows some numbers I picked for you.
-         I rolled actual dice for these, so they're *guaranteed* to
-         be random.
-     """)
-     st.altair_chart((chart).interactive(), use_container_width=True)
+    #Chart in Altair and display in Streamlit
+    chart = alt.Chart(df).mark_point().encode(
+        x='Timestamp:T',
+        y='Temperature:Q',
+        tooltip=[alt.Tooltip('Timestamp', timeUnit="hoursminutesseconds"), 'Temperature']
+    ).properties(
+        width=750,
+        height=550
+    )
+    st.altair_chart((chart).interactive(), use_container_width=True)
